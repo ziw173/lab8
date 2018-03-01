a4cb4fd25ebba591c93d2bb9f43ec2c2242f0887
Ziqi Wu <ziw173@ucsd.edu>
2018-03-01 10:55:40 -0800

    up

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
   initializePage();
 })
 
+$(function(){
+	$( "div.box" ).bind("taphold", tapholdHandler);
+	function tapholderHandler(event){
+		$(event.target).addClass("taphold");
+	}
+});
 /*
  * Function that is called when the document is ready.
  */
@@ -23,4 +29,17 @@ function initGestures() {
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+}
+
+//init jQuery gestures
+function initGestures(){
+	//add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholderHandler);
+		functioj tapholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: "+ targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
